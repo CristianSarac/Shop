02a82526cfcb7c4a3ac63733984e60072ccf20fb
RDGary <dragan.raicici@gmail.com>
2013-11-11 18:42:38 +0100

    implemented design and a few functionalities

---
+++ App_Code/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public double Price { get; set; }
+    public string Roast { get; set; }
+    public string Country { get; set; }
+    public string Image { get; set; }
+    public string Review { get; set; }
+
+    public Product(int id, string name, string type, double price, string roast, string country, string image, string review)
+    {
+        Id = id;
+        Name = name;
+        Type = type;
+        Price = price;
+        Roast = roast;
+        Country = country;
+        Image = image;
+        Review = review;
+    }
+
+    public Product(string name, string type, double price, string roast, string country, string image, string review)
+    {
+        Name = name;
+        Type = type;
+        Price = price;
+        Roast = roast;
+        Country = country;
+        Image = image;
+        Review = review;
+    }
+}
---
+++ Pages/Product.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+
+public partial class Pages_Product : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       FillPage();
+    }
+
+   private void FillPage()
+   {
+       ArrayList productList = new ArrayList();
+
+       if(!IsPostBack)
+       {
+           productList = ConnectionClass.GetProductByType("%");
+       }
+       else
+       {
+           productList = ConnectionClass.GetProductByType(DropDownList1.SelectedValue);
+       }
+       
+       StringBuilder sb = new StringBuilder();
+
+       foreach (Product product in productList)
+       {
+           sb.Append(
+               string.Format(
+                   @"<table class='productTable'>
+            <tr>
+                <th rowspan='6' width='150px'><img runat='server' src='{6}' /></th>
+                <th width='50px'>Name: </td>
+                <td>{0}</td>
+            </tr>
+
+            <tr>
+                <th>Type: </th>
+                <td>{1}</td>
+            </tr>
+
+            <tr>
+                <th>Price: </th>
+                <td>{2} $</td>
+            </tr>
+
+            <tr>
+                <th>Roast: </th>
+                <td>{3}</td>
+            </tr>
+
+            <tr>
+                <th>Origin: </th>
+                <td>{4}</td>
+            </tr>
+
+            <tr>
+                <td colspan='2'>{5}</td>
+            </tr>           
+            
+           </table>",
+                   product.Name, product.Type, product.Price, product.Roast, product.Country, product.Review, product.Image));
+
+           lblOuput.Text = sb.ToString();
+
+       }
+
+       
+   }
+   protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+   {
+       FillPage();
+   }
+}
