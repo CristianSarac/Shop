c2bf4740f7a7bf0f802cdc623947c676471d394e
RDGary <dragan.raicici@gmail.com>
2013-11-12 01:30:39 +0100

    Add to cart si remove to cart implemented, to some extent. Dynamical display of products.

--- Pages/Shop.aspx.cs
+++ Pages/Shop.aspx.cs
@@ -8,9 +8,12 @@ namespace Pages
 {
     public partial class Pages_Shop : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
             GenerateControls();
+            Authenticate();
+            String user = Session["login"].ToString();
         }
 
         protected void btnOk_Click(object sender, EventArgs e)
@@ -29,6 +32,7 @@ namespace Pages
             btnOk.Visible = false;
             btnCancel.Visible = false;
             lblResult.Visible = false;
+            
 
         }
 
@@ -36,54 +40,79 @@ namespace Pages
         {
             Authenticate();
             GenerateReview();
+
+
         }
 
         //Fill page with dynamic controls showing products in database
         private void GenerateControls()
         {
             //Get all productObjects from database
-            ArrayList productList = ConnectionClass.GetProductByType("%");
+           ArrayList productList = ConnectionClass.GetProductByType("%");
 
-            foreach (Product product in productList)
-            {
-                //Create Controls
-                Panel productPanel = new Panel();
-                Image image = new Image { ImageUrl = product.Image, CssClass = "ProductsImage" };
-                Literal literal = new Literal { Text = "<br />" };
-                Literal literal2 = new Literal { Text = "<br />" };
-                Label lblName = new Label { Text = product.Name, CssClass = "ProductsName" };
-                Label lblPrice = new Label
-                                     {
-                                         Text = String.Format("{0:0.00}", product.Price + "<br />"),
-                                         CssClass = "ProductsPrice"
-                                     };
-                TextBox textBox = new TextBox
-                                      {
-                                          ID = product.Id.ToString(),
-                                          CssClass = "ProductsTextBox",
-                                          Width = 60,
-                                          Text = "0"
-                                      };
 
-                //Add validation so only numbers can be entered into the textfields
-                RegularExpressionValidator regex = new RegularExpressionValidator
-                                                       {
-                                                           ValidationExpression = "^[0-9]*",
-                                                           ControlToValidate = textBox.ID,
-                                                           ErrorMessage = "Please enter a number."
-                                                       };
+//           foreach (Product product in productList)
+//           {
+//               //Create Controls
+//               StringBuilder sb = new StringBuilder();
+//               Panel productPanel = new Panel();
+//               Image image = new Image { ImageUrl = product.Image, CssClass = "ProductsImage" };
+//               Literal literal = new Literal { Text = "<br />" };
+//               Literal literal2 = new Literal { Text = "<br />" };
+//               Label lblName = new Label { Text = product.Name, CssClass = "ProductsName" };
+//               Label lblPrice = new Label
+//                                    {
+//                                        Text = String.Format("{0:0.00}", product.Price + "<br />"),
+//                                        CssClass = "ProductsPrice"
+//                                    };
+//               TextBox textBox = new TextBox
+//                                     {
+//                                         ID = product.Id.ToString(),
+//                                         CssClass = "ProductsTextBox",
+//                                         Width = 60,
+//                                         Text = "0"
+//                                     };
 
-                //Add controls to Panels
-                productPanel.Controls.Add(image);
-                productPanel.Controls.Add(literal);
-                productPanel.Controls.Add(lblName);
-                productPanel.Controls.Add(literal2);
-                productPanel.Controls.Add(lblPrice);
-                productPanel.Controls.Add(textBox);
-                productPanel.Controls.Add(regex);
+//               //Add validation so only numbers can be entered into the textfields
+//               RegularExpressionValidator regex = new RegularExpressionValidator
+//                                                      {
+//                                                          ValidationExpression = "^[0-9]*",
+//                                                          ControlToValidate = textBox.ID,
+//                                                          ErrorMessage = "Please enter a number."
+//                                                      };
 
-                pnlProducts.Controls.Add(productPanel);
-            }
+//               //PayPal Button
+
+
+//               sb.Append(string.Format(
+//                 @"<form method='post' action='https://www.paypal.com/cgi-bin/webscr'>
+//                    <input type='hidden' name='cmd' value='_cart'>
+//                    <input type='hidden' name='add' value='1'>
+//                    <input type='hidden' name='amount' value='5'>
+//                    <input type='hidden' name='item_name' value='Ceva'>
+//                    <input type='hidden' name='currency_code' value='USD'>
+//                    <input type='hidden' name='return' value=''>
+//                    <input type='image' src='http://www.paypalobjects.com/en_US/i/btn/x-click-but22.gif' 
+//                           border='0' name='submit' width='87' height='23' 
+//                           alt='Make payments with PayPal - it's fast, free and secure!'>
+//                        </form>
+//                  <script type='text/javascript' src= '../JavaScript/minicart.min.js'>paypal.minicart.render();</script>
+//                  ",
+//                        textBox.Text, product.Price, product.Name, "../JavaScript/minicart.min.js"));
+//               Label lblOutput = new Label { Text = sb.ToString() };
+
+//               //Add controls to Panels
+//               productPanel.Controls.Add(image);
+//               productPanel.Controls.Add(literal);
+//               productPanel.Controls.Add(lblName);
+//               productPanel.Controls.Add(literal2);
+//               productPanel.Controls.Add(lblPrice);
+//               productPanel.Controls.Add(textBox);
+//               productPanel.Controls.Add(regex);
+//               productPanel.Controls.Add(lblOutput);
+
+//               pnlProducts.Controls.Add(productPanel);
+//           }
         }
 
         //Returns a list of all orders placed in textboxes
@@ -147,7 +176,7 @@ namespace Pages
             sb.Append(String.Format(@"<tr>
                                         <td><b>Total: </b></td>
                                         <td><b>{0} $ </b></td>
-                                      </tr>", totalAmount));           
+                                      </tr>", totalAmount));
             sb.Append("</table>");
 
             //Export data and make Controls visible
